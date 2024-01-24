cf7eea045e77ff93a7430bce8363d07a1975fec2
skillz <goslowbutneverstop@gmail.com>
2024-01-23 22:19:31 -0500

    Channel object is broken in pytube, refractored some of the code will try fixing channel obj

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from pytube import Playlist, YouTube
+from pytube import Playlist, YouTube, Channel
 from pytube.cli import on_progress
 import os
 import time
@@ -7,9 +7,12 @@ import json
 class bot:
     def __init__(self):
         """instantiate bot with a link and define what the link is pointing to"""
-        self.object = input("Downloading a (V)ideo or a (P)laylist? ").strip().lower()
-        if self.object not in ('p','v'):
-            self.object = None
+        while True:
+            self.object = input("Downloading a (V)ideo, a (P)laylist or the content of a (C)hannel? ").strip().lower()
+            if self.object not in ('p','v','c'):
+                print("Please enter a valid choice")
+            else:
+                break
         self.url = input("Paste the url of the object (the playlist,channel or video) ").strip()
         while True:
             user_input = input("Would you want to authenticate your YouTube Account?(y/n)").lower().strip()
@@ -21,41 +24,43 @@ class bot:
 
     def chose_function(self):
         """Chose what function to call"""
-        
         if self.auth == True:
             self.oauth()
         if self.object == 'p':
             self.download_playlist()
         if self.object == 'v':
             self.download_video()
+        if self.object == 'c':
+            self.download_channel()
 
     def oauth(self):
-        """Authetication function"""
-        print("As part of the authentication process YtDl will now download a sample video")
+        """Auth function"""
+        print("As part of the authentication process YtDl will download a sample video")
         print("Please follow the instructions")
         YouTube("https://www.youtube.com/watch?v=K4TOrB7at0Y",use_oauth=True,allow_oauth_cache=True,on_progress_callback=on_progress).streams.order_by("resolution").first().download()
+    
+    def audio_prompt(self):
+        """Prompt for audio"""
+        while True:
+            user_input = input("Do you want to download the video's audio only? (N/y)").lower().strip()
+            if user_input in ('y','n',''):
+                return user_input == 'y'
+            else:
+                print("Please make a valid choice")
 
-    def download_playlist(self):
+
+    def download_playlist(self, url=None, only_audio=None):
         """Downloading the playlist in a folder with same title"""
         playlist = Playlist(self.url)
-        only_audio = None
-        
-        #asking user if he needs video or only_audio
-        while not only_audio:
-            user_input = input("Would you like to download the audio without the video?(N/y)").lower().strip()
-            if user_input in ('y','n',''):
-                audio = user_input == 'y'
-                break
-            else:
-                print("please make a valid choice")
+        only_audio = self.audio_prompt()
 
         #user needs to make sure the playlist is set to Public on YouTube
         try:
             directory = playlist.title
         except:
-            print(f"This playlist is not publicly available. Please make sure the playlist is publicly available or use the Authetication option. If you did authetify you can ignore this message")
+            print(f"This playlist is not publicly available. Please make sure the playlist is publicly available or use the Authetication option. If you did, you can ignore this message")
             directory = "Playlist"
-            #return 0
+        
         #creating folder to download videos will fail if user doesn't have appropriate permissions
         try:
             os.mkdir(f'{directory}')
@@ -69,45 +74,52 @@ class bot:
 
         #downloading videos in playlist
         for vid in playlist.videos:
-            print(f"downloading {vid.title}\n")
             vid.register_on_progress_callback(on_progress)
-            if p_oauth:
-                vid.use_oauth = True
-                vid.allow_oauth_cache = True
-            if only_audio:
-                stream = vid.streams.filter(only_audio=only_audio).last()
-                stream.download()
-                title = stream.title
-                ext = stream.mime_type.split('/')[1]
-                os.rename(f"{title}.{ext}", f"{title}_audio.{ext}")
-            else:
-                stream = vid.streams.order_by("resolution").filter(progressive=True).last()
-                stream.download()
+            self.download_video(vid, only_audio)
+
+            #print(f"downloading {vid.title}\n")
+            #vid.register_on_progress_callback(on_progress)
+            #if p_oauth:
+            #    vid.use_oauth = True
+            #    vid.allow_oauth_cache = True
+            #if only_audio:
+            #    stream = vid.streams.filter(only_audio=only_audio).last()
+            #    stream.download()
+            #    title = stream.title
 
 
-    def download_video(self):
+    def download_video(self, vid=None, only_audio=None):
         """downloads single video, Uses Progressive download"""
-        vid = YouTube(self.url,on_progress_callback=on_progress)
-        only_audio = None
+        if not vid:
+            vid = YouTube(self.url,on_progress_callback=on_progress)
         
-        while not only_audio:
-            user_input = input("Would you like to only download the audio?(N/y)").lower().strip()
-            if user_input in ('y','n',''):
-                only_audio = user_input == 'y'
-                break
-            else:
-                print("please make a valid choice")
-        
+        if only_audio is None:
+            only_audio = self.audio_prompt()
+       
         if only_audio:
             stream = vid.streams.filter(only_audio=only_audio).filter().last()
-            stream.download()
             title = stream.title
+            print(f"Downloading {title}")
+            stream.download()
             ext = stream.mime_type.split('/')[1]
             os.rename(f"{title}.{ext}", f"{title}_audio.{ext}")
         else:
             stream = vid.streams.order_by("resolution").filter(progressive=True).last()
+            title = stream.title
+            print(f"Downloading {title}")
             stream.download()
+       
+    def download_channel(self):
+        """Downloads the entire content of a Youtube Channel"""
+        c = Channel(self.url)
+        only_audio = self.audio_prompt()
         
+        for video in c.videos:
+            video.register_on_progress_callback(on_progress)
+            self.download_video(video,only_audio)
+            
+
+
 if __name__ == "__main__":
     bot = bot()
     bot.chose_function()
